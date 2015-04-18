18d6ea93aa3b900adf4e7ce428badfc66e1619d7
Alex Beznos <beznosa@yahoo.com>
2015-04-18 13:58:13 +0300

    add index page

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Uawc
   class Application < Rails::Application
+    Rails.application.config.assets.precompile += %w( *.css )
+    Rails.application.config.assets.precompile += %w( *.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'public/index'
+  root to: 'public#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
